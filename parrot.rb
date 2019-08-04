4c6aa19f84e5c839f0b48a219019a141f52d633f
STEVEN RUGG <rugg.stevenw@gmail.com>
2019-08-04 06:38:35 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,9 +1,15 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
+<<<<<<< HEAD
 
+=======
+>>>>>>> 3ca23deccd1867a2bb8c38fdf4daff6d9463d224
 def parrot(noise="Squawk!")
   puts "#{noise}"
   return noise
 end
+<<<<<<< HEAD
  
+=======
+>>>>>>> 3ca23deccd1867a2bb8c38fdf4daff6d9463d224
